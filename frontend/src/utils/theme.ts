97d923a66197f428473a9fad9baf5752e0198cac
Bilal Aabkari <bilalaabkari13@gmail.com>
2024-10-11 00:33:35 +0200

    setup

---
+++ frontend/src/utils/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#1B2021" },
+    secondary: { main: "#E8EBF7" },
+    background: { default: "#f5f5f5" },
+  },
+});
+
+export default theme;
